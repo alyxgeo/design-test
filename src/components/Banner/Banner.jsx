78f7481354b78094f65879dc7260cf003c0f9534
alyxgeo <alyxgeo76@gmail.com>
2023-12-11 15:07:49 +0530

    update

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,14 +4,14 @@ const Banner = () => {
         flex justify-center "
         >
 
-            <div className="flex flex-col gap-28  items-center">
-                <div className="mt-12">
-                    <div className="text-[#D7FFA5] font-normal text-6xl font-sree">Discover </div>
-                    <div className="text-white fonr-normal text-6xl font-sree">our New Products for You</div>
+            <div className="lg:flex flex-col gap-28  items-center px-5 lg:px-0">
+                <div className="lg:mt-12">
+                    <div className="text-[#D7FFA5] font-normal lg:text-6xl font-sree text-3xl">Discover </div>
+                    <div className="text-white fonr-normal lg:text-6xl font-sree text-3xl">our New Products for You</div>
                 </div>
 
 
-                <div className="flex justify-center items-center py-3 px-2 gap-2 bg-[#73A336] w-fit text-base rounded-md">
+                <div className=" mt-10 lg:mt-0 flex justify-center items-center py-3 px-2 gap-2 bg-[#73A336] w-fit text-base rounded-md">
                     <div className="text-white font-sree text-xl">Shop Now</div>
                     <div>
                         <img src="/icons/arrow-btn-icon.png" alt="arrow-btn" />
@@ -20,14 +20,14 @@ const Banner = () => {
 
 
 
-                <div className="flex gap-5 ">
+                <div className="lg:flex lg:gap-5 mt-5 lg:mt-0 ">
                     <div className="flex flex-col gap-4  items-center">
                         <p className="border-b text-white font-sree text-sm">The Ordinary</p>
                         <img src="public/images/banner-ord.png" alt="cart-img" className="w-fit" />
                     </div>
 
 
-                    <div className="flex flex-col gap-4 items-center mt-20">
+                    <div className="flex flex-col gap-4 items-center lg:mt-20">
                         <p className="border-b text-white font-sree text-sm">The Body Shop</p>
                         <img src="public/images/banner-shop.png" alt="cart-img" className="w-fit" />
                     </div>
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
 
   return (
 
-    <div className='bg-[#8EB226] flex justify-between px-5 py-4 '>
+    <div className='bg-[#8EB226] flex justify-between px-5 py-4  '>
 
       <div className="flex gap-2  items-center">
         <img src='/images/logo.png' alt='logo' className='w-7 h-7' />
@@ -12,7 +12,7 @@ const Navbar = () => {
       </div>
 
 
-      <div className="flex items-center font-medium gap-5 text-white font-inter text-base">
+      <div className="lg:flex items-center font-medium gap-5 text-white font-inter text-base hidden ">
         <div>Shop</div>
         <div>Product</div>
         <div>Contact</div>
@@ -20,7 +20,7 @@ const Navbar = () => {
       </div>
 
 
-      <div className="px-5 flex gap-3 items-center  bg-neutral-100 rounded-md ">
+      <div className="px-5 lg:flex gap-3 items-center  bg-neutral-100 rounded-md hidden ">
         <img src="public/icons/search-icon.png" alt="search" />
         <input placeholder="Search for products" className="text-sm bg-transparent py-2" />
       </div>
